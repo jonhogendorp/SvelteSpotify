cca439e51a0fe4eceb1b128e6881b8a2ca6326b8
Jon Hogendorp <64735813+jonhogendorp@users.noreply.github.com>
2024-04-02 12:17:15 +0200

    counter update

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,18 @@
 <script>
-  let count = 0
-  const increment = () => {
-    count += 1
-  }
+	let count = 0;
+
+	$: string = `You have clicked ${count} times.  `;
+
+	$: if (count > 5) {
+		console.log(`The count is ${count}`);
+	}
+	function increment() {
+		count += 1;
+	}
 </script>
 
 <button on:click={increment}>
-  count is {count}
+	Clicks {count}
 </button>
+
+<h3>{string}</h3>
